ec61ac57f6ce7ba118e25326abc6b16147cea692
hongchanho98 <hongchanho98@gmail.com>
2021-08-18 17:04:09 +0900

    더미작업

--- DB/DDL/DDL_CREATE.sql
+++ DB/DDL/DDL_CREATE.sql
@@ -18,7 +18,7 @@ CREATE TABLE tblclass (
     classseq       NUMBER NOT NULL,
     hostseq        NUMBER NOT NULL,
     title          VARCHAR2(200) NOT NULL,
-    content        VARCHAR2(2000) NOT NULL,
+    content        VARCHAR2(4000) NOT NULL,
     categorybig    VARCHAR2(100) NOT NULL,
     categorysmall  VARCHAR2(100) NOT NULL,
     location       VARCHAR2(200) NOT NULL,
@@ -184,8 +184,8 @@ ALTER TABLE tblhostscrap
         REFERENCES tbluser ( userseq );
 
 ALTER TABLE tblhostscrap
-    ADD CONSTRAINT fk_host_hostscrap FOREIGN KEY ( hostseq )
-        REFERENCES tblhost ( hostseq );
+    ADD CONSTRAINT fk_host_hostscrap FOREIGN KEY (hostseq)
+        REFERENCES tblhost (hostseq);
 
 ALTER TABLE tblclassimage
     ADD CONSTRAINT fk_class_classimage FOREIGN KEY ( classseq )
