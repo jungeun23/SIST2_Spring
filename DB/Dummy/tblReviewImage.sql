dd4460fd5c871b707e086db5bb5c759a90fe2ce9
hongchanho98 <hongchanho98@gmail.com>
2021-08-19 14:43:44 +0900

    업데이트

---
+++ DB/Dummy/tblReviewImage.sql
@@ -0,0 +1,63 @@
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 1, classReview1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 2, classReview2-1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 2, classReview2-2.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 2, classReview2-3.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 3, classReview3.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 4, classReview4.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 5, classReview5.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 6, classReview6-1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 6, classReview6-2.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 7, classReview7.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 8, classReview8-1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 8, classReview8-2.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 9, classReview9.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 10, classReview10.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 11, classReview11.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 12, classReview12.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 13, classReview13.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 14, classReview14.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 15, classReview15.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 16, classReview16.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 17, classReview17.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 18, classReview18.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 19, classReview19.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 20, classReview20.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 21, classReview21.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 22, classReview22.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 23, classReview23.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 24, classReview24-1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 24, classReview24-2.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 25, classReview25.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 26, classReview26-1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 26, classReview26-2.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 27, classReview27.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 28, classReview28.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 29, classReview29.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 30, classReview30.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 31, classReview31.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 32, classReview32-1.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 32, classReview32-2.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 33, classReview33.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 34, classReview34.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 35, classReview35.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 36, classReview36.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 37, classReview37.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 38, classReview38.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 39, classReview39.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 40, classReview40.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 41, classReview41.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 42, classReview42.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 43, classReview43.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 44, classReview44.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 45, classReview45.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 46, classReview46.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 47, classReview47.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 48, classReview48.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 49, classReview49.png);
+insert into tblReviewImage (reviewImageSeq, classReviewSeq, reviewImage) values (seqReviewImage.nextVal, 50, classReview50.png);
+
+
+
+
+
+
