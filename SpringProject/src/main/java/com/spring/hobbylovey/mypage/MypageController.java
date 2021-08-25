2debb90f0f5b17f19518e284fe0a05d18c1e8dbc
mmmhmh <algusl1212@naver.com>
2021-08-25 15:54:09 +0900

    커밋

--- SpringProject/src/main/java/com/spring/hobbylovey/mypage/MypageController.java
+++ SpringProject/src/main/java/com/spring/hobbylovey/mypage/MypageController.java
@@ -22,10 +22,11 @@ public class MypageController {
 
 		String seq = "14"; //임시 회원번호 - 로그인 기능구현 후 수정해야함
 		
-		//수강신청내역	//디비 연결 에러있음.................해결해야함
-//		List<SignUpClassDTO> list = dao.getSignUpClass(seq);
-//		
-//		req.setAttribute("list", list);
+		//수강신청내역	
+		List<SignUpClassDTO> list = dao.getSignUpClass(seq);
+		
+		System.out.println("list 사이즈: " + list.size());
+		req.setAttribute("list", list);
 		
 		return "member.mypage";
 		
