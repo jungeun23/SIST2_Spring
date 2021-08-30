eebd58f96e7b1d3dba4ff0f420829eaab3029b19
jungeun23 <mnms11@naver.com>
2021-08-30 22:22:39 +0900

    에디터 이미지

--- SpringProject/src/main/java/com/spring/hobbylovey/host/HostController.java
+++ SpringProject/src/main/java/com/spring/hobbylovey/host/HostController.java
@@ -22,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.google.gson.JsonObject;
+
 @Controller
 public class HostController {
 
@@ -46,90 +48,101 @@ public class HostController {
 	}
 
 	@RequestMapping(value = "/host/classenrollok.action", method = { RequestMethod.POST })
-	public String classenrollok(HttpServletRequest req, HttpServletResponse resp, HttpSession session, ClassDTO dto) {
+	public String classenrollok(HttpServletRequest req, HttpServletResponse resp, HttpSession session, ClassDTO dto,MultipartHttpServletRequest multiFile, MultipartFile upload) {
 
-
+		System.out.println(dto);
+		
 		return "host.classenrollok";
 	}
 
 	@RequestMapping(value = "/host/imageUpload.action", method = RequestMethod.POST)
-	public void imageUpload(HttpServletRequest request, HttpServletResponse response,
-			MultipartHttpServletRequest multiFile, @RequestParam MultipartFile upload) throws Exception {
-		// 랜덤 문자 생성
-		UUID uid = UUID.randomUUID();
-		OutputStream out = null;
-		PrintWriter printWriter = null;
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html;charset=utf-8");
-		try {
-			String fileName = upload.getOriginalFilename();
-			byte[] bytes = upload.getBytes();
-
-			String path = "C:\\Users\\cksgh\\Desktop\\백업\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\SpringProject\\resources\\images";
-			String ckUploadPath = path + uid + "_" + fileName;
-			File folder = new File(path); // 해당 디렉토리 확인
-			if (!folder.exists()) {
-				try {
-					folder.mkdirs();
-				} catch (Exception e) {
-					e.getStackTrace();
+	public String imageUpload(HttpServletRequest req, HttpServletResponse resp, MultipartHttpServletRequest multiFile, MultipartFile upload)  throws Exception {
+		
+				JsonObject json = new JsonObject();
+				PrintWriter printWriter = null;
+				OutputStream out = null;
+				MultipartFile file = upload;
+				if(file != null){
+					if(file.getSize() > 0 && !file.getName().isBlank()){
+						if(file.getContentType().toLowerCase().startsWith("image/")){
+							try{
+								String fileName = file.getOriginalFilename();
+								System.out.println("fileName: " + fileName);
+								byte[] bytes = file.getBytes();
+								String uploadPath = req.getSession().getServletContext().getRealPath("/files");
+								System.out.println(uploadPath);
+								File uploadFile = new File(uploadPath);
+								if(!uploadFile.exists()){
+									uploadFile.mkdirs();
+								}
+								//fileName = UUID.randomUUID().toString();
+								uploadPath = uploadPath + "/" + fileName;
+								out = new FileOutputStream(new File(uploadPath));
+		                        out.write(bytes);
+		                        
+		                        printWriter = resp.getWriter();
+		                        resp.setContentType("text/html");
+		                        String fileUrl = req.getContextPath() + "/files/" + fileName;
+		                        
+		                        // json 데이터로 등록
+		                        // {"uploaded" : 1, "fileName" : "test.jpg", "url" : "/img/test.jpg"}
+		                        // 이런 형태로 리턴이 나가야함.
+		                        json.addProperty("uploaded", 1);
+		                        json.addProperty("fileName", fileName);
+		                        json.addProperty("url", fileUrl);
+		                        
+		                        System.out.println("fileurl: " + fileUrl);
+		                        
+		                        printWriter.println(json);
+		                    }catch(IOException e){
+		                        e.printStackTrace();
+		                    }finally{
+		                        if(out != null){
+		                            out.close();
+		                        }
+		                        if(printWriter != null){
+		                            printWriter.close();
+		                        }		
+							}
+						}
+					}
 				}
+				return null;
 			}
-			out = new FileOutputStream(new File(ckUploadPath));
-			out.write(bytes);
-			out.flush();
-			String callback = request.getParameter("CKEditorFuncNum");
-			printWriter = response.getWriter();
-			String fileUrl = "/host/ckImgSubmit.do?uid=" + uid + "&fileName=" + fileName;
-			printWriter.println("{\"filename\" : \"" + fileName + "\",\"uploaded\" : 1, \"url\":\"" + fileUrl + "\"}");
-			printWriter.flush();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-				if (printWriter != null) {
-					printWriter.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return;
-	}
-
-	@RequestMapping(value="/host/ckImgSubmit.action") 
-	public void ckSubmit(@RequestParam(value="uid") String uid , @RequestParam(value="fileName") String fileName , HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	@RequestMapping(value = "/host/ckImgSubmit.action")
+	public void ckSubmit(@RequestParam(value = "uid") String uid, @RequestParam(value = "fileName") String fileName,
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = "C:\\Users\\cksgh\\Desktop\\백업\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\SpringProject\\resources\\images";
 		String sDirPath = path + uid + "_" + fileName;
 		File imgFile = new File(sDirPath);
-		if(imgFile.isFile()){
-			byte[] buf = new byte[1024]; 
-			int readByte = 0; int length = 0; 
+		if (imgFile.isFile()) {
+			byte[] buf = new byte[1024];
+			int readByte = 0;
+			int length = 0;
 			byte[] imgBuf = null;
 			FileInputStream fileInputStream = null;
 			ByteArrayOutputStream outputStream = null;
 			ServletOutputStream out = null;
-			try{ fileInputStream = new FileInputStream(imgFile);
-			outputStream = new ByteArrayOutputStream();
-			out = response.getOutputStream();
-			while((readByte = fileInputStream.read(buf)) != -1){
-				outputStream.write(buf, 0, readByte); 
-			} imgBuf = outputStream.toByteArray();
-			length = imgBuf.length;
-			out.write(imgBuf, 0, length);
-			out.flush();
-			}catch(IOException e){
-				
-			}finally {
+			try {
+				fileInputStream = new FileInputStream(imgFile);
+				outputStream = new ByteArrayOutputStream();
+				out = response.getOutputStream();
+				while ((readByte = fileInputStream.read(buf)) != -1) {
+					outputStream.write(buf, 0, readByte);
+				}
+				imgBuf = outputStream.toByteArray();
+				length = imgBuf.length;
+				out.write(imgBuf, 0, length);
+				out.flush();
+			} catch (IOException e) {
+
+			} finally {
 				outputStream.close();
 				fileInputStream.close();
 				out.close();
-				}
-			} 
+			}
+		}
 	}
 
 }
